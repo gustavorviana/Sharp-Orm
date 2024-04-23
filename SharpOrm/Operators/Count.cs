a5cbb6c1eada2d2bc68ab34d16d9d91f9f638e46
Gustavo Viana <gustavorviana@outlook.com>
2024-04-23 08:52:09 -0300

    Added: Count class operation

---
+++ SharpOrm/Operators/Count.cs
@@ -0,0 +1,13 @@
+﻿namespace SharpOrm.Operators
+{
+    public class Count : Column
+    {
+        public Count(string column) : base(new SqlExpression($"COUNT({column})"))
+        {
+        }
+
+        public Count(string column, string alias) : base(new SqlExpression($"COUNT({column}) {alias}"))
+        {
+        }
+    }
+}
