696f106c06cb81b5672502fe628e2a9f2c277615
Gustavo Viana <gustavorviana@outlook.com>
2024-09-24 19:23:57 -0300

    Addition of validations, corrections in functions, and addition of comments in the manual table mapping.

--- SharpOrm/Builder/TableMap.cs
+++ SharpOrm/Builder/TableMap.cs
@@ -7,16 +7,43 @@ using System.Reflection;
 
 namespace SharpOrm.Builder
 {
+    /// <summary>
+    /// Represents a mapping between a type <typeparamref name="T"/> and a database table.
+    /// </summary>
+    /// <typeparam name="T">The type to be mapped to the table.</typeparam>
     public class TableMap<T>
     {
         private static readonly BindingFlags Binding = BindingFlags.Instance | BindingFlags.Public;
         private readonly List<MemberTreeNode> Nodes = new List<MemberTreeNode>();
         private TableInfo table;
 
-        public string Name { get; set; }
+        private string _name;
+        /// <summary>
+        /// Gets or sets the name of the table. 
+        /// Throws an <see cref="InvalidOperationException"/> if the table has already been created.
+        /// </summary>
+        public string Name
+        {
+            get => this._name;
+            set
+            {
+                if (this.table != null)
+                    throw new InvalidOperationException("It is not possible to change the table name after it has been created.");
 
+                this._name = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the translation registry used for the mapping.
+        /// </summary>
         public TranslationRegistry Registry { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TableMap{T}"/> class.
+        /// Automatically maps the properties and fields of the specified type <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="registry">The translation registry to be used for the mapping.</param>
         public TableMap(TranslationRegistry registry)
         {
             this.Name = typeof(T).Name;
@@ -51,11 +78,33 @@ namespace SharpOrm.Builder
             return rootNode;
         }
 
-        public ColumnMapInfo Property(Expression<Func<T, object>> expression, string columnName)
+        /// <summary>
+        /// Sets the key property for the table using the specified expression.
+        /// </summary>
+        /// <param name="expression">An expression selecting the property to be used as the key.</param>
+        /// <returns>The current <see cref="TableMap{T}"/> instance.</returns>
+        public TableMap<T> HasKey(Expression<Func<T, object>> expression)
         {
-            return this.Property(expression).SetColumn(columnName);
+            this.Property(expression).SetKey(true);
+            return this;
         }
 
+        /// <summary>
+        /// Maps a property to a specific column name in the table.
+        /// </summary>
+        /// <param name="expression">An expression selecting the property to be mapped.</param>
+        /// <param name="columnName">The name of the column in the table.</param>
+        /// <returns>A <see cref="ColumnMapInfo"/> representing the mapped column.</returns>
+        public ColumnMapInfo Property(Expression<Func<T, object>> expression, string columnName)
+        {
+            return this.Property(expression).HasColumnName(columnName);
+        }
+
+        /// <summary>
+        /// Maps a property using the specified expression.
+        /// </summary>
+        /// <param name="expression">An expression selecting the property to be mapped.</param>
+        /// <returns>A <see cref="ColumnMapInfo"/> representing the mapped property.</returns>
         public ColumnMapInfo Property(Expression<Func<T, object>> expression)
         {
             var path = PropertyPathVisitor.GetPropertyPaths(expression);
@@ -93,6 +142,10 @@ namespace SharpOrm.Builder
             }
         }
 
+        /// <summary>
+        /// Builds the table mapping and registers it with the translation registry.
+        /// </summary>
+        /// <returns>A <see cref="TableInfo"/> representing the built table mapping.</returns>
         public TableInfo Build()
         {
             if (this.Nodes.Count == 0) return null;
