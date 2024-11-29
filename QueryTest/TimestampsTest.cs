a813e0401a22cdf72bc3d12928682cc48066b57e
Gustavo Viana <gustavorviana@outlook.com>
2024-11-28 21:27:33 -0300

    Added mapped table with timestamp

--- QueryTest/TimestampsTest.cs
+++ QueryTest/TimestampsTest.cs
@@ -1,6 +1,7 @@
 ﻿using BaseTest.Models;
 using BaseTest.Utils;
 using SharpOrm;
+using SharpOrm.Builder;
 
 namespace QueryTest
 {
@@ -28,12 +29,44 @@ namespace QueryTest
             using var fallback = RegisterFallback();
             using var query = new Query<AddressWithTimeStamp>();
             query.Where(x => x.Id, 1);
-            query.Update(new AddressWithTimeStamp(1) { Name = "Test", UpdatedAt = updateTime });
+            query.Update(new AddressWithTimeStamp(1) { Name = "Test" });
 
             Assert.Equal("UPDATE [AddressWithTimeStamp] SET [Name] = @p1, [Street] = NULL, [UpdatedAt] = @p2 WHERE [Id] = 1", fallback.ToString());
             var dbParams = fallback.GetParameters();
 
             Assert.IsType<DateTime>(dbParams[1].Value);
+        }
+
+        [Fact]
+        public void MappedWithUpdatedOnlyInsert()
+        {
+            var mapper = new TableMap<Address>(Translation);
+            mapper.HasTimeStamps(null, "UpdatedAt");
+            mapper.HasKey(x => x.Id).Build();
+
+            using var fallback = RegisterFallback();
+            using var query = new Query<Address>();
+            query.Insert(new Address(1) { Name = "Test" });
+
+            Assert.Equal("INSERT INTO [Address] ([Id], [Name], [Street], [UpdatedAt]) VALUES (1, @p1, NULL, @p2)", fallback.ToString());
+            var dbParams = fallback.GetParameters();
+
+            Assert.IsType<DateTime>(dbParams[1].Value);
+        }
+
+        [Fact]
+        public void MappedWithCreateOnlyUpdate()
+        {
+            var mapper = new TableMap<Address>(Translation);
+            mapper.HasTimeStamps("CreatedAt", null);
+            mapper.HasKey(x => x.Id).Build();
+
+            using var fallback = RegisterFallback();
+            using var query = new Query<Address>();
+            query.Where(x => x.Id, 1);
+            query.Update(new Address(1) { Name = "Test" });
+
+            Assert.Equal("UPDATE [Address] SET [Name] = @p1, [Street] = NULL WHERE [Id] = 1", fallback.ToString());
         }
     }
 }
