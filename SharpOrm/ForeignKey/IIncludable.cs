e99595459eee1b3a2026a6684016d7d61556dc98
Gustavo Viana <gustavorviana@outlook.com>
2025-06-21 19:05:17 -0300

    Added join name to includable

--- SharpOrm/ForeignKey/IIncludable.cs
+++ SharpOrm/ForeignKey/IIncludable.cs
@@ -1,13 +1,16 @@
-﻿using SharpOrm.DataTranslation;
+﻿using SharpOrm.Builder;
+using SharpOrm.DataTranslation;
 
 namespace SharpOrm.ForeignKey
 {
     public interface IIncludable<out TEntity, out TProperty>
     {
+        DbName Name { get; }
     }
 
     internal interface IIncludable
     {
+        DbName Name { get; }
         ForeignKeyRegister Register { get; }
         ForeignKeyNodeBase Node { get; }
     }
--- SharpOrm/ForeignKey/Includable.cs
+++ SharpOrm/ForeignKey/Includable.cs
@@ -1,9 +1,12 @@
-﻿using SharpOrm.DataTranslation;
+﻿using SharpOrm.Builder;
+using SharpOrm.DataTranslation;
 
 namespace SharpOrm.ForeignKey
 {
     internal class Includable<TEntity, TProperty> : IIncludable<TEntity, TProperty>, IIncludable
     {
+        public DbName Name => Node.Name;
+
         public ForeignKeyRegister Register { get; }
         public ForeignKeyNodeBase Node { get; }
 
